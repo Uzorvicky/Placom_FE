c64b3fab1ee358268f5ae2fe73f3b6c7bacc811b
uzorvicky <uzorvicky@yahoo.com>
2025-06-20 23:47:24 +0100

    Update page.tsx

--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -4,6 +4,7 @@ import UseOTP from "@/hooks/useOTP";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { toast } from "react-toastify";
+import { Loader2Icon } from "lucide-react"
 import { useRouter } from "next/navigation";
 import Card from "@/shared/authCard";
 import styled from "styled-components";
@@ -39,16 +40,15 @@ interface ApiError {
 
 const VerifyForgotPassword: React.FC = () => {
     const router = useRouter();
+    const [finalValue, setFinalValue] = useState<string>('')
 
-    //   const { mutate, isPending, data, isError } = useMutation<ApiResponse, ApiError, VerifyForgotPasswordPayload>(
-    //     (payload) => api.verifyForgotPassword(payload)
-    //   );
+    const mutation = useMutation<ApiResponse, ApiError, VerifyForgotPasswordPayload>({
+        mutationFn: (payload) => api.verifyForgotPassword(payload)
+    });
 
-    //   const { mutate: resendOtp } = useMutation<ApiResponse, ApiError, ResendOtpPayload>(
-    //     (payload) => api.resendOtp(payload)
-    //   );
-
-    //   const [finalValue, inputValues, handleChange, handleKeyDown, handlePaste] = useOTP();
+      const resendOtp = useMutation<ApiResponse, ApiError, ResendOtpPayload>({
+      mutationFn:  (payload) => api.resendOtp(payload)
+});
     const [isSuccess, setIsSuccess] = useState<boolean>(false);
     const [inputError, setInputError] = useState<string | null>(null);
     const [btnText, setBtnText] = useState<string>("Continue");
@@ -72,105 +72,89 @@ const VerifyForgotPassword: React.FC = () => {
         return `Code expired...`;
     }, []);
 
-    //   const handleResendOtp = async (e: MouseEvent<HTMLAnchorElement>): Promise<void> => {
-    //     e.preventDefault();
-    //     const email = localStorage.getItem("reset_password_email");
+      const handleResendOtp = async (e: MouseEvent<HTMLAnchorElement>): Promise<void> => {
+        e.preventDefault();
+        const email = localStorage.getItem("reset_password_email");
 
-    //     if (!email) {
-    //       toast.error("Email not found. Please go back to forgot password.");
-    //       return;
-    //     }
+        if (!email) {
+          toast.error("Email not found. Please go back to forgot password.");
+          return;
+        }
 
-    //     try {
-    //       resendOtp(
-    //         { email: email },
-    //         {
-    //           onSuccess: ({ message }: ApiResponse) => {
-    //             toast.success(message);
-    //           },
-    //           onError: (error: ApiError) => {
-    //             console.log(error, ":ERROR RESPONSE resend");
-    //             toast.error( error?.message || "Failed to resend OTP");
-    //           },
-    //         }
-    //       );
-    //     } catch (error) {
-    //       const err = error as ApiError;
-    //       toast.error(err?.response?.data?.message || err?.message || "Failed to resend OTP");
-    //     }
-    //   };
+        try {
+          resendOtp.mutate(
+            { email: email },
+            {
+              onSuccess: ({ message }: ApiResponse) => {
+                toast.success(message);
+              },
+              onError: (error: ApiError) => {
+                console.log(error, ":ERROR RESPONSE resend");
+                toast.error( error?.message || "Failed to resend OTP");
+              },
+            }
+          );
+        } catch (error) {
+          const err = error as ApiError;
+          toast.error(err?.message || "Failed to resend OTP");
+        }
+      };
 
-    //   const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
-    //     e.preventDefault();
+    const handleSubmit = async (): Promise<void> => {
 
-    //     if (finalValue?.length < 6) {
-    //       setInputError("Please enter your OTP");
-    //       return;
-    //     }
+        if (finalValue?.length < 6) {
+            setInputError("Please enter your OTP");
+            return;
+        }
 
-    //     const email = localStorage.getItem("reset_password_email");
+        const email = localStorage.getItem("reset_password_email");
 
-    //     // If no email was found on local storage go back to /auth/forgotpassword
-    //     if (!email) {
-    //       return router.push('/auth/forgot-password');
-    //     }
+        // If no email was found on local storage go back to /auth/forgotpassword
+        if (!email) {
+            return router.push('/auth/forgot-password');
+        }
 
-    //     try {
-    //       const payload: VerifyForgotPasswordPayload = {
-    //         otp: finalValue,
-    //         email: email,
-    //       };
+        try {
+            const payload: VerifyForgotPasswordPayload = {
+                otp: finalValue,
+                email: email,
+            };
 
-    //       mutate(payload, {
-    //         onSuccess: (data: ApiResponse) => {
-    //           setIsSuccess(true);
-    //           toast.success(data?.message);
-    //           // Redirect user to reset password
-    //           router.push("/auth/reset-password");
-    //         },
-    //         onError: (error: ApiError) => {
-    //           const errorMessage = error?.message || "Verification failed";
-    //           setBtnText(errorMessage);
-    //           setTimeout(() => {
-    //             setBtnText("Continue");
-    //           }, 2000);
+            mutation.mutate(payload, {
+                onSuccess: (data: ApiResponse) => {
+                    setIsSuccess(true);
+                    toast.success(data?.message);
+                    // Redirect user to reset password
+                    router.push("/auth/reset-password");
+                },
+                onError: (error: ApiError) => {
+                    const errorMessage = error?.message || "Verification failed";
+                    setBtnText(errorMessage);
+                    setTimeout(() => {
+                        setBtnText("Continue");
+                    }, 2000);
 
-    //           toast.error(errorMessage);
-    //         },
-    //       });
-    //     } catch (error) {
-    //       const err = error as ApiError;
-    //       toast.error("Error verifying OTP, Try again");
-    //       setBtnText("Error! Try Again");
-    //       setTimeout(() => {
-    //         setBtnText("Continue");
-    //       }, 2000);
-    //     }
-    //   };
+                    toast.error(errorMessage);
+                },
+            });
+        } catch (error) {
+            const err = error as ApiError;
+            toast.error(err?.message || "Error verifying OTP, Try again");
+            setBtnText("Error! Try Again");
+            setTimeout(() => {
+                setBtnText("Continue");
+            }, 2000);
+        }
+    };
 
-    //   const handleInputChange = (event: ChangeEvent<HTMLInputElement>, index: number): void => {
-    //     handleChange(event, index);
-    //   };
 
-    //   const handleInputKeyDown = (event: KeyboardEvent<HTMLInputElement>, index: number): void => {
-    //     handleKeyDown(event, index);
-    //   };
-
-    //   const handleInputFocus = (): void => {
-    //     setInputError(null);
-    //   };
-
-    //   const handlePasteEvent = (event: ClipboardEvent<HTMLDivElement>): void => {
-    //     handlePaste(event);
-    //   };
 
     return (
         <AuthLayout register={true}>
             <VerifyForgotPasswordWrapper>
                 <Card
-                // isLoading={isPending}
+                    isLoading={mutation.isPending}
                 >
-
                     <div className="w-full flex flex-col space-y-4 mb-5 md:mb-10 text-[#535353]">
                         <span style={{
                             fontSize: "22px",
@@ -182,6 +166,7 @@ const VerifyForgotPassword: React.FC = () => {
                         <H3 style={{ fontSize: "17px" }}>{"Please enter the code below"}</H3>
                     </div>
                     <UseOTP
+                        onComplete={(x) => setFinalValue(x)}
                     />
                     {inputError !== null && (
                         <div className="flex justify-start w-full mb-3">
@@ -199,28 +184,25 @@ const VerifyForgotPassword: React.FC = () => {
                             {formatTime(time)}
                         </span>
                     </Section>
-                    <ButtonWrapper>
+                    <ButtonWrapper className="space-y-2">
                         <Button
                             variant={'primary'}
                             size={'lg'}
                             className="w-full bg-[#0F6862] rounded-none !py-6 px-4 text-lg"
-
-                        //   onClick={handleSubmit}
+                            disabled={time <= 0}
+                            onClick={handleSubmit}
                         >
-                            {false ?
-                                "Verifying your account..." : btnText}
+                            {/* {false ?
+                                "Verifying your account..." : btnText} */}
+                            {mutation.isPending ? <Loader2Icon className="animate-spin" /> : false}
+                            {btnText}
                         </Button>
 
-
-
-                        {/* <Button
-              noBorder
-              isLoading={isPending}
-              buttonName={"Back"}
-              textColor={"#535353"}
-              bgColor={"transparent"}
-              onClick={() => router.back()}
-            /> */}
+                        <Button
+                            disabled={mutation.isPending}
+                            size={'md'}
+                            className="bg-[#535353] text-white rounded-none !py-5 px-4"
+                            onClick={() => router.back()}>{"Back"}</Button>
                     </ButtonWrapper>
                 </Card>
             </VerifyForgotPasswordWrapper>
@@ -229,31 +211,6 @@ const VerifyForgotPassword: React.FC = () => {
 };
 
 export default VerifyForgotPassword;
-
-const StyledInput = styled.input`
-  font-size: 3rem;
-  text-align: center;
-  border-radius: 15px;
-  color: #000;
-  background: #ffffff;
-  box-sizing: border-box;
-  caret-color: transparent;
-  border: 1.5px solid #b3b3b3;
-
-  &:focus {
-    outline-color: #0c8ce9;
-  }
-
-  @media screen and (max-width: 800px) {
-    border-radius: 10px;
-    font-size: 1.5rem;
-    font-weight: 700;
-  }
-`;
-
-const FlexibleDiv = styled.div`
-  justify-content: space-between;
-`;
 
 const Section = styled.div`
   width: 100%;
@@ -293,5 +250,5 @@ export const ButtonWrapper = styled.div`
 `;
 
 export const StyledLink = styled(Link)`
-  color: #f68b1e;
+  color: #0F6862;
 `;
